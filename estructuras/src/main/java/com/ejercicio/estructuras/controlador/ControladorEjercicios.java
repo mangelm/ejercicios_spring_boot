0ba2d4e68a7afe5383bb4d82585963a182a67e34
M. Angel Moreno <miguelangelmorenodelgadot@gmail.com>
2024-11-23 00:52:45 +0100

    Solución del ejercicio 3

--- estructuras/src/main/java/com/ejercicio/estructuras/controlador/ControladorEjercicios.java
+++ estructuras/src/main/java/com/ejercicio/estructuras/controlador/ControladorEjercicios.java
@@ -8,15 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControladorEjercicios {
 	
 	
-	@PostMapping("/parImpar")
-	public String ejercicio1(@RequestParam int numero ) {
+	@PostMapping("/promedioCalificaciones")
+	public String ejercicio3(@RequestParam String calificaciones ) {
 		String ret = "";
-		if(numero % 2 == 0) {
-			ret+="Es par el " + numero;
-		}else {
-			ret+="Es impar el " + numero;
+		double promedio = 0.0;
+		String[] notas = calificaciones.split(",");
+		double suma = 0;
+		
+		for (String nota: notas) {
+			suma += Double.parseDouble(nota.trim());
 		}
 		
+		promedio = suma / notas.length;
+		
+		if (promedio >= 5) {
+			ret += "El promedio del usuario es " + promedio + " por lo tanto está aprobado."; 
+		}else {
+			ret += "El promedio del usuario es " + promedio + " por lo tanto está suspendido.";
+		}
 		
 		return "<h1>"+ ret +"</h1>";
 	}
